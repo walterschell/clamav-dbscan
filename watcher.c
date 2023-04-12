bef2e5063b8a5b2506ee3be172cb68cb5594817c
Walter Schell <walterschell@users.noreply.github.com>
2023-04-12 15:27:50 -0400

    valgrind clean

--- watcher.c
+++ watcher.c
@@ -10,6 +10,16 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <cJSON.h>
+#include <uv.h>
+#include <sys/queue.h>
+#include <assert.h>
+#include <errno.h>
+
+#define DEFAULT_UNIX_DOMAIN_SOCKET_PATH "/tmp/fswatcher.sock"
+#define UNUSED __attribute__((unused))
 
 int safe_readlink(const char *pathname, char **buf)
 {
@@ -27,13 +37,13 @@ int safe_readlink(const char *pathname, char **buf)
         free(_buf);
         _buf = (char *) malloc(size);
         assert(NULL != _buf);
-        int linksize = readlink(pathname, _buf, size);
-        if (linksize == -1)
+        ssize_t linksize = readlink(pathname, _buf, size);
+        if (linksize < 0)
         {
             perror("readlink");
             return -1;
         }
-        if (linksize < size)
+        if ((size_t) linksize < size)
         {
             _buf[linksize] = '\0';
             *buf = _buf;
@@ -43,6 +53,74 @@ int safe_readlink(const char *pathname, char **buf)
     }
 }
 
+int bind_to_unix_domain_socket(char *path)
+{
+    int fd = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+    if (fd == -1)
+    {
+        perror("socket");
+        return -1;
+    }
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(struct sockaddr_un));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path, sizeof(addr.sun_path) - 1);
+    if (bind(fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_un)) == -1)
+    {
+        if (errno == EADDRINUSE)
+        {
+            int client_fd = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+            if (client_fd == -1)
+            {
+                perror("socket");
+                close(fd);
+                return -1;
+            }
+            if (connect(client_fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_un)) == -1)
+            {
+                if (unlink(path) == -1)
+                {
+                    printf("Socket already existed and we failed to unlink it\n");
+                    perror("unlink");
+                    close(fd);
+                    return -1;
+                }
+                if (bind(fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_un)) == -1)
+                {
+                    perror("bind after unlink");
+                    close(fd);
+                    return -1;
+                }
+            }
+            else
+            {
+                close(client_fd);
+                close(fd);
+                printf("Socket is actively being used by another process\n");
+                return -1;
+            }
+        }
+        else
+        {
+            perror("bind");
+            close(fd);
+            return -1;
+        }
+    }
+    if (listen(fd, 10) == -1)
+    {
+        perror("listen");
+        close(fd);
+        return -1;
+    }
+    if (chmod(path, 0777) == -1)
+    {
+        perror("chmod");
+        close(fd);
+        return -1;
+    }
+    return fd;
+}
 
 char *filepath_from_fanotify_even_metadata(struct fanotify_event_metadata *metadata, int mount_fd) 
 {
@@ -96,10 +174,354 @@ char *filepath_from_fanotify_even_metadata(struct fanotify_event_metadata *metad
 }
 
 typedef enum operation_type_t {
-    OP_ADD,
+    OP_CLOSE_FOR_WRITE,
     OP_REMOVE,
-    OP_UNKNOWN
+    OP_MOVE_FROM,
+    OP_MOVE_TO,
+    OP_UNKNOWN,
 } operation_type_t;
+
+const char *operation_type_to_string[] = {
+    [OP_CLOSE_FOR_WRITE] = "close_for_write",
+    [OP_REMOVE] = "remove",
+    [OP_MOVE_FROM] = "move_from",
+    [OP_MOVE_TO] = "move_to",
+    [OP_UNKNOWN] = "unknown",};
+
+
+typedef struct json_entry_t {
+    cJSON *json;
+    STAILQ_ENTRY(json_entry_t) entries;
+} json_entry_t;
+
+typedef struct refcounted_uv_buffer_t {
+    int refcount;
+    uv_buf_t buf;
+} refcounted_uv_buffer_t;
+
+refcounted_uv_buffer_t * refcounted_uv_buffer_from_string(char *str){
+    refcounted_uv_buffer_t *buf = (refcounted_uv_buffer_t *) malloc(sizeof(refcounted_uv_buffer_t));
+    assert(NULL != buf);
+    buf->refcount = 1;
+    buf->buf.base = str;
+    buf->buf.len = strlen(str);
+    return buf;
+}
+
+void refcounted_uv_buffer_ref(refcounted_uv_buffer_t *buf){
+    buf->refcount++;
+}
+
+void refcounted_uv_buffer_unref(refcounted_uv_buffer_t *buf){
+    buf->refcount--;
+    if (buf->refcount == 0){
+        free(buf->buf.base);
+        free(buf);
+    }
+}
+
+
+
+typedef struct loop_state_t {
+    int exit_code;
+    int fs_fd;
+    STAILQ_HEAD(json_queue_t, json_entry_t) json_queue;
+    uv_idle_t *send_jsons_handle;
+    int *clients;
+    size_t clients_count;
+    size_t clients_capacity;
+    char *compare_path;
+} loop_state_t;
+
+
+void loop_push_json(uv_loop_t *loop, cJSON *json)
+{
+    loop_state_t *state = (loop_state_t *) loop->data;
+    json_entry_t *entry = (json_entry_t *) malloc(sizeof(json_entry_t));
+    entry->json = json;
+    STAILQ_INSERT_TAIL(&state->json_queue, entry, entries);
+}
+
+cJSON * loop_pop_json(uv_loop_t *loop)
+{
+    loop_state_t *state = (loop_state_t *) loop->data;
+    json_entry_t *entry = STAILQ_FIRST(&state->json_queue);
+    if (entry == NULL)
+    {
+        return NULL;
+    }
+    cJSON *json = entry->json;
+    STAILQ_REMOVE_HEAD(&state->json_queue, entries);
+    free(entry);
+    return json;
+}
+
+bool loop_has_jsons(uv_loop_t *loop)
+{
+    loop_state_t *state = (loop_state_t *) loop->data;
+    return STAILQ_FIRST(&state->json_queue) != NULL;
+}
+
+void loop_add_client(uv_loop_t *loop, int client)
+{
+    loop_state_t *state = (loop_state_t *) loop->data;
+    if (state->clients_capacity == 0)
+    {
+        state->clients_capacity = 4;
+        state->clients = (int *) malloc(sizeof(int) * state->clients_capacity);
+        for (size_t i = 0; i < state->clients_capacity; i++)
+        {
+            state->clients[i] = -1;
+        }
+    }
+    else if (state->clients_count == state->clients_capacity)
+    {
+        size_t old_capacity = state->clients_capacity;
+        state->clients_capacity *= 2;
+        state->clients = (int *) realloc(state->clients, sizeof(int) * state->clients_capacity);
+        for (size_t i = old_capacity; i < state->clients_capacity; i++)
+        {
+            state->clients[i] = -1;
+        }
+    }
+    for(size_t i = 0; i < state->clients_capacity; i++)
+    {
+        if (state->clients[i] == -1)
+        {
+            state->clients[i] = client;
+            state->clients_count++;
+            printf("Added %d as a client\n", client);
+            return;
+        }
+    }
+    printf("ERROR Should not happen\n");
+ }
+
+void loop_remove_client(uv_loop_t *loop, int client)
+{
+    loop_state_t *state = (loop_state_t *) loop->data;
+    for (size_t i = 0; i < state->clients_capacity; i++)
+    {
+        if (state->clients[i] == client)
+        {
+            state->clients[i] = -1;
+            state->clients_count--;
+            break;
+        }
+    }
+    printf("Clients %p\n", (void *) state->clients);
+
+}
+
+void free_handle(uv_handle_t *handle)
+{
+    free(handle);
+}
+
+#define UV_TERMINATE_LOOP(loop, reason) \
+    ((loop_state_t *) loop->data)->exit_code = reason; \
+    uv_stop(loop); \
+    return;
+
+
+void on_client_writeable(uv_poll_t *handle, UNUSED int status, UNUSED int events)
+{
+    int clientfd = handle->u.fd;
+    refcounted_uv_buffer_t *buf = (refcounted_uv_buffer_t *) handle->data;
+    printf("Sending to client %d\n", clientfd);
+    if (send(clientfd, buf->buf.base, buf->buf.len, 0) == -1)
+    {
+        if (errno != EPIPE)
+        {
+            perror("send");
+        }
+        close(clientfd);
+        loop_remove_client(handle->loop, clientfd);
+    }
+    else
+    {
+        printf("Sent to client %d\n", clientfd);
+    }
+    refcounted_uv_buffer_unref(buf);
+    uv_poll_stop(handle);
+    uv_close((uv_handle_t *) handle, free_handle);
+
+}
+
+void send_jsons(uv_idle_t *handle)
+{
+    uv_loop_t *loop = handle->loop;
+    loop_state_t *loop_state = (loop->data);
+    cJSON *json = loop_pop_json(loop);
+    if (json == NULL)
+    {
+        return;
+    }
+    char *json_string = cJSON_Print(json);
+    cJSON_Delete(json);
+    printf("%s\n", json_string);
+    refcounted_uv_buffer_t *buf = refcounted_uv_buffer_from_string(json_string);
+
+    printf("Sending to %zu clients\n", loop_state->clients_count);
+
+    int *client = loop_state->clients;
+    for (size_t i = 0; i < loop_state->clients_count; i++)
+    {
+        while (*client == -1)
+        {
+            assert(client < &loop_state->clients[loop_state->clients_capacity]);
+            client++;
+        }
+        printf("Preparing Sending to client %d\n", *client);
+        uv_poll_t *req = (uv_poll_t *) malloc(sizeof(uv_poll_t));
+        uv_poll_init(loop, req, *client);
+        req->u.fd = *client;
+        req->data = buf;
+        refcounted_uv_buffer_ref(buf);
+        uv_poll_start(req, UV_WRITABLE, on_client_writeable);
+        client++;
+    }
+
+    refcounted_uv_buffer_unref(buf);
+    if (!loop_has_jsons(loop))
+    {
+        uv_idle_stop(handle);
+    }
+}
+
+void on_unix_domain_connection_attempt(uv_poll_t *handle, UNUSED int status, UNUSED int events)
+{
+    struct sockaddr_un addr;
+    socklen_t addrlen = sizeof(addr);
+
+
+    int fd = accept(handle->u.fd, (struct sockaddr *) &addr, &addrlen);
+    if (fd == -1)
+    {
+        perror("accept");
+        UV_TERMINATE_LOOP(handle->loop, 1);
+    }
+    printf("Accepted client connection\n");
+    loop_add_client(handle->loop, fd);
+}
+
+
+void on_fs_event(uv_poll_t *handle, UNUSED int status, UNUSED int events)
+{
+        loop_state_t *loop_state = (loop_state_t *) handle->loop->data;
+        char buf[4096];
+        int buflen = read(handle->u.fd, buf, sizeof(buf));
+        struct fanotify_event_metadata *metadata = (struct fanotify_event_metadata *)  buf;
+        //printf("Read %d bytes\n", buflen);
+        cJSON *json = cJSON_CreateObject();
+        cJSON *json_events = cJSON_CreateArray();
+        cJSON_AddItemToObject(json, "events", json_events);
+        while (FAN_EVENT_OK(metadata, buflen))
+        {
+            if (metadata->vers != FANOTIFY_METADATA_VERSION)
+            {
+                printf("Wrong metadata version: %d\n", metadata->vers);
+                UV_TERMINATE_LOOP(handle->loop, 1);
+            }
+            assert(metadata->fd == FAN_NOFD);
+
+            char *filepath = filepath_from_fanotify_even_metadata(metadata, ((loop_state_t *) handle->loop->data)->fs_fd);
+            if (filepath == NULL)
+            {
+                printf("Failed to get filepath\n");
+                UV_TERMINATE_LOOP(handle->loop, 1);
+            }
+            if (strncmp(filepath, loop_state->compare_path, strlen(loop_state->compare_path)) != 0)
+            {
+                printf("Skipping %s\n", filepath);
+                metadata = FAN_EVENT_NEXT(metadata, buflen);
+                free(filepath);
+                continue;
+            }
+            operation_type_t op = OP_UNKNOWN;
+            if (metadata->mask & FAN_CLOSE_WRITE)
+            {
+                op = OP_CLOSE_FOR_WRITE;
+            }
+
+
+            if (metadata->mask & FAN_MOVED_TO)
+            {
+                op = OP_MOVE_TO;
+            }
+
+            if (metadata->mask & FAN_DELETE) 
+            {
+                op = OP_REMOVE;
+            }
+            if (metadata->mask & FAN_MOVED_FROM)
+            {
+                op = OP_MOVE_FROM;
+            }
+
+            metadata = FAN_EVENT_NEXT(metadata, buflen);
+            cJSON *json_event = cJSON_CreateObject();
+            cJSON_AddStringToObject(json_event, "path", filepath);
+            cJSON_AddStringToObject(json_event, "operation", operation_type_to_string[op]);
+            cJSON_AddItemToArray(json_events, json_event);
+            free(filepath);
+        }
+        if (cJSON_GetArraySize(json_events) == 0)
+        {
+            cJSON_Delete(json);
+            return;
+        }
+        loop_push_json(handle->loop, json);
+        uv_idle_start(((loop_state_t *) handle->loop->data)->send_jsons_handle, send_jsons);
+}
+
+void open_handles_cb(uv_handle_t *handle, UNUSED void *arg)
+{
+    if (!uv_is_closing(handle))
+    {
+        switch (handle->type)
+        {
+        case UV_POLL:
+            printf("Closing poll handle\n");
+            uv_poll_stop((uv_poll_t *) handle);
+            if (((uv_poll_t *) handle)->poll_cb == on_client_writeable)
+            {
+                if (((uv_poll_t *) handle)->data != NULL)
+                {
+                    refcounted_uv_buffer_unref(((uv_poll_t *) handle)->data);
+                }
+                uv_close(handle, free_handle);
+            }
+            else
+            {
+                uv_close(handle, NULL);
+            }
+        break;
+        case UV_IDLE:
+            printf("Closing idle handle\n");
+            uv_idle_stop((uv_idle_t *) handle);
+            uv_close(handle, NULL);
+        break;
+        default:
+            printf("Unknown handle of type %d\n", handle->type);
+        break;
+        }
+    }
+}
+
+
+void sigint_handler(uv_signal_t *handle, UNUSED int signum)
+{
+    uv_loop_t *loop = handle->loop;
+    printf("SIGINT received\n");
+    uv_signal_stop(handle);
+    uv_close((uv_handle_t *) handle, NULL);
+    uv_walk(loop, open_handles_cb, NULL);
+}
+
+
+
+
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -107,6 +529,19 @@ int main(int argc, char **argv) {
         return 1;
     }
     char *path = argv[1];
+    char *canonical_path = realpath(path, NULL);
+    if (canonical_path == NULL)
+    {
+        printf("Failed to get canonical path\n");
+        perror("realpath");
+        return 1;
+    }
+
+    char *compare_path = calloc(1, strlen(canonical_path) + 2);
+    strcpy(compare_path, canonical_path);
+    strcat(compare_path, "/");
+    free(canonical_path);
+
     int fs_fd = open(path, O_DIRECTORY | O_RDONLY);
     if (fs_fd == -1)
     {
@@ -127,58 +562,79 @@ int main(int argc, char **argv) {
         perror("fanotify_mark");
         return 1;
     }
-    char buf[4096];
-    while (true)
+
+    uv_idle_t idle_handle;
+
+    uv_loop_t *loop = uv_loop_new();
+    loop_state_t loop_state = {
+        .exit_code = 0,
+        .send_jsons_handle = &idle_handle,
+        .fs_fd = fs_fd,
+        .compare_path = compare_path,
+    };
+    STAILQ_INIT(&loop_state.json_queue);
+    loop->data = &loop_state;
+    uv_idle_init(loop, &idle_handle);
+
+    uv_poll_t poll_handle;
+    poll_handle.data = NULL;
+    uv_poll_init(loop, &poll_handle, fd);
+    poll_handle.u.fd = fd;
+    uv_poll_start(&poll_handle, UV_READABLE, on_fs_event);
+
+
+
+    int listenfd = bind_to_unix_domain_socket(DEFAULT_UNIX_DOMAIN_SOCKET_PATH);
+    if (listenfd == -1)
     {
-        int buflen = read(fd, buf, sizeof(buf));
-        struct fanotify_event_metadata *metadata = (struct fanotify_event_metadata *)  buf;
-        //printf("Read %d bytes\n", buflen);
-        while (FAN_EVENT_OK(metadata, buflen))
+        printf("Failed to bind to unix domain socket\n");
+        return 1;
+    }
+
+    uv_poll_t unix_domain_poll_handle;
+    unix_domain_poll_handle.data = NULL;
+    unix_domain_poll_handle.u.fd = listenfd;
+    uv_poll_init(loop, &unix_domain_poll_handle, listenfd);
+    uv_poll_start(&unix_domain_poll_handle, UV_READABLE, on_unix_domain_connection_attempt);
+
+    uv_signal_t sigint_handle;
+    uv_signal_init(loop, &sigint_handle);
+    uv_signal_start(&sigint_handle, sigint_handler, SIGINT);
+
+
+    uv_run(loop, UV_RUN_DEFAULT);
+
+    unlink(DEFAULT_UNIX_DOMAIN_SOCKET_PATH);
+
+    printf("Cliends at end: %p\n", (void *) loop_state.clients);
+    for (size_t i = 0; i < loop_state.clients_capacity; i++)
+    {
+        if (loop_state.clients[i] != -1)
         {
-            if (metadata->vers != FANOTIFY_METADATA_VERSION)
-            {
-                printf("Wrong metadata version: %d\n", metadata->vers);
-                return 1;
-            }
-            assert(metadata->fd == FAN_NOFD);
-
-            char *filepath = filepath_from_fanotify_even_metadata(metadata, fs_fd);
-            if (filepath == NULL)
-            {
-                printf("Failed to get filepath\n");
-                return 1;
-            }
-            operation_type_t op = OP_UNKNOWN;
-            if (metadata->mask & FAN_CLOSE_WRITE)
-            {
-                printf("A file was closed for writing: %s\n", filepath);
-                op = OP_ADD;
-            }
-
-            if (metadata->mask & FAN_MOVED_TO)
-            {
-                printf("A file was moved to: %s\n", filepath);
-                op = OP_ADD;
-            }
-
-            if (metadata->mask & FAN_DELETE) 
-            {
-                printf("A file was removed: %s\n", filepath);
-                op = OP_REMOVE;
-            }
-            if (metadata->mask & FAN_MOVED_FROM)
-            {
-                printf("A file was moved from: %s\n", filepath);
-                op = OP_REMOVE;
-            }
-            if (op == OP_UNKNOWN)
-            {
-                printf("Unknown event\n");
-            }
-
-            metadata = FAN_EVENT_NEXT(metadata, buflen);
+            close(loop_state.clients[i]);
         }
     }
+
+    free(loop_state.clients);
+    loop_state.clients = NULL;
+
+    while (loop_has_jsons(loop))
+    {
+        cJSON *json = loop_pop_json(loop);
+        cJSON_Delete(json);
+    }
+    free(loop_state.compare_path);
+
+    close(fd);
+    close(fs_fd);
+
+    if (uv_loop_close(loop) != 0)
+    {
+        printf("Failed to close loop\n");
+        return 1;
+    }
+
+    free(loop);
 
     return 0;
 
